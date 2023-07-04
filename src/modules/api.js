deba69767049d003c8b885f6880055220587b8a2
rubydevi <ariesabyss@gmail.com>
2023-07-04 15:28:55 +0530

    Create app id

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,4 +1,20 @@
 const baseURL = 'https://www.themealdb.com/api/json/v1/1/';
+const involvementAPIBaseURL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/';
+
+// Create a new app and retrieve the appID
+
+export async function createApp() {
+  const storedAppId = localStorage.getItem('appId');
+  if (storedAppId && storedAppId === '1FNl9krFuHr2YmoEXWQu') {
+    return storedAppId;
+  }
+
+  localStorage.setItem('appId', '1FNl9krFuHr2YmoEXWQu');
+  return '1FNl9krFuHr2YmoEXWQu';
+}
+
+// Export the base URL for the meal API
+export { baseURL };
 
 // List of meal
 export async function getRegionWiseMeal() {

8134e04e4b884e8cc28c46a8fa8ae3ccde4c03fa
“sadaf <sadaf.batekzai54321@gmial.com>
2023-07-05 12:52:33 +0430

    Add the category on popup

--- src/modules/popup.js
+++ src/modules/popup.js
@@ -1,37 +1,46 @@
+const baseURL = 'https://www.themealdb.com/api/json/v1/1/';
 const addComment = async (id, data) => {
   const meal = data.find((ele) => ele.idMeal === id);
   const mealContainer = document.getElementById('meal');
   mealContainer.style.display = 'flex';
 
+  // Fetch meal details from the API using the id
+  const response = await fetch(`${baseURL}lookup.php?i=${id}`);
+  const mealDetails = await response.json();
+
   const mealElement = document.createElement('div');
   mealElement.className = 'meal-list';
   mealElement.innerHTML = '';
 
+  const mealCategory = mealDetails.meals[0].strCategory;
+
   mealElement.innerHTML = `
-  <div class="close-btn"><button type="button" class="btn recipe-close-btn">
-  <i class="fas fa-times"></i>
-</button>
-</div>
-<div class="meal-image-comment">
-<div class="meal-list-comment">
-  <img src="${meal.strMealThumb}" alt="" class="meal-image">
-  <div class="comment-meal">
-    <h2 class="meal-name">${meal.strMeal}</h2>
-    <h3>Comments</h3>
-    <ul id="comments-list"></ul>
-  </div>
-</div>
-<h3 class="comment-part">Add Comments</h3>
-<div class="form">
-  <input type="text" class="name" id="name" placeholder="Name">
-</div><br>
-<div class="form">
-  <textarea name="textarea" id="textArea" cols="40" rows="10"></textarea>
-</div>
-<div class="form">
-  <button type="submit" id="submit">Submit</button>
-</div>
-</div>
+    <div class="close-btn">
+      <button type="button" class="btn recipe-close-btn">
+        <i class="fas fa-times"></i>
+      </button>
+    </div>
+    <div class="meal-image-comment">
+      <div class="meal-list-comment">
+        <img src="${meal.strMealThumb}" alt="" class="meal-image">
+        <div class="comment-meal">
+          <h2 class="meal-name">${meal.strMeal}</h2>
+          <h3>Category: ${mealCategory}</h3>
+          <h3>Comments</h3>
+          <ul id="comments-list"></ul>
+        </div>
+      </div>
+      <h3 class="comment-part">Add Comments</h3>
+      <div class="form">
+        <input type="text" class="name" id="name" placeholder="Name">
+      </div><br>
+      <div class="form">
+        <textarea name="textarea" id="textArea" cols="40" rows="10"></textarea>
+      </div>
+      <div class="form">
+        <button type="submit" id="submit">Submit</button>
+      </div>
+    </div>
   `;
 
   mealContainer.appendChild(mealElement);

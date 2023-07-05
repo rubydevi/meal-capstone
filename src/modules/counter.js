5514f8dae40ba0a4cb803d58e97989c5f5cc72f1
rubydevi <ariesabyss@gmail.com>
2023-07-05 15:53:08 +0530

    Export countItems as default

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,4 +1,6 @@
-export const countItems = () => {
+const countItems = () => {
   const itemList = document.getElementById('item-list');
   return itemList.children.length;
 };
+
+export default countItems;
